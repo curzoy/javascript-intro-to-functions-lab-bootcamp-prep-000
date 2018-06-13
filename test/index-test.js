dd608f2295ff13efde99421ea2d0ff971655f7d2
bahez <bahezmohammad164@gmail.com>
2018-06-13 12:23:33 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,10 +1,9 @@
 
-function shout(string){
-  
-  return
-  string.toUpperCase()
-  
-}
+describe('shout(string)', function() {
+  it('receives one argument and returns it in all caps', function() {
+    expect(shout('hello')).toEqual('HELLO')
+  })
+})
 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
